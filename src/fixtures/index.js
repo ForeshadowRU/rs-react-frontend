0166f5b3490ee1f3a3721b428bbaa363e6c3d341
Jesper <MrJesper@yandex.ru>
2018-06-29 01:05:43 +0600

    Added Navbar and empty Vacancy file
    Signed-off-by: Jesper <MrJesper@yandex.ru>

---
+++ src/fixtures/index.js
@@ -0,0 +1,7 @@
+export default fixtures = [
+    {id: 1, name: "JavaScript Developer", author: "Union Technologies LLC", description: "Desc"},
+    {id: 1, name: "JavaScript Developer", author: "Union Technologies LLC", description: "Desc"},
+    {id: 1, name: "JavaScript Developer", author: "Union Technologies LLC", description: "Desc"},
+    {id: 1, name: "JavaScript Developer", author: "Union Technologies LLC", description: "Desc"},
+    {id: 1, name: "JavaScript Developer", author: "Union Technologies LLC", description: "Desc"}
+]
